a65ddfc5326154ee4aee02294bdcb80821579aaa
devkiloton <dev.kiloton@gmail.com>
2021-05-18 09:38:20 -0300

    update 2

--- Survivor mode/Assets/C# scripts/ControlaInimigo.cs
+++ Survivor mode/Assets/C# scripts/ControlaInimigo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ControlaInimigo : MonoBehaviour
 {
     public GameObject Jogador;
-    public float Velocity = 5;
+    public float Velocity = 4;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +19,18 @@ public class ControlaInimigo : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 Direction = Jogador.transform.position - transform.position;
-        GetComponent<Rigidbody>().MovePosition
-            (GetComponent<Rigidbody>().position +
-            Direction.normalized * Velocity * Time.deltaTime);
+        float distance = Vector3.Distance(transform.position, Jogador.transform.position);
+        if (distance > 2.5)
+        {
+            Vector3 Direction = Jogador.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition
+                (GetComponent<Rigidbody>().position +
+                Direction.normalized * Velocity * Time.deltaTime);
+        
+
+        Quaternion NewRotation = Quaternion.LookRotation(Direction);
+
+        GetComponent<Rigidbody>().MoveRotation(NewRotation);
+        }
     }
 }
